057d9cc202f63ffaef477733c4d14cfea3f531fc
natepage <nathan.page86@gmail.com>
2026-03-09 11:34:59 +0000

    [EasyAdminAddons] Fix CrudAddonsResolverListener.php

--- src/Listener/CrudAddonsResolverListener.php
+++ src/Listener/CrudAddonsResolverListener.php
@@ -3,39 +3,43 @@ declare(strict_types=1);
 
 namespace NatePage\EasyAdminAddons\Listener;
 
+use EasyCorp\Bundle\EasyAdminBundle\Config\Option\EA;
+use EasyCorp\Bundle\EasyAdminBundle\Factory\ControllerFactory;
+use NatePage\EasyAdminAddons\Config\CrudAddons;
 use NatePage\EasyAdminAddons\Controller\AbstractCrudController;
 use NatePage\EasyAdminAddons\Provider\CrudAddonsProviderInterface;
+use NatePage\Utils\Helper\StringHelper;
 use Psr\Log\LoggerInterface;
-use Symfony\Component\HttpKernel\Event\ControllerEvent;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
 
 final readonly class CrudAddonsResolverListener
 {
     public function __construct(
+        private ControllerFactory $controllerFactory,
         private CrudAddonsProviderInterface $crudAddonsProvider,
         private LoggerInterface $logger,
     ) {
     }
 
-    public function __invoke(ControllerEvent $event): void
+    public function __invoke(RequestEvent $event): void
     {
-        $controller = $event->getController();
+        $request = $event->getRequest();
+        $controllerFqcn = $request->attributes->get(EA::CRUD_CONTROLLER_FQCN);
+        $actionName = $request->attributes->get(EA::CRUD_ACTION);
 
-        if (\is_array($controller)) {
-            $controller = $controller[0] ?? null;
-        }
-
-        if ($controller instanceof AbstractCrudController) {
-            $this->crudAddonsProvider->setResolver(static function () use ($controller) {
-                return $controller->configureCrudAddons();
-            });
+        if (StringHelper::isEmpty($controllerFqcn)) {
+            $this->logger->debug('Not an easy-admin route, skipping CRUD addons resolver');
 
             return;
         }
 
-        $this->logger->debug(\sprintf(
-            'Controller "%s" is not an instance of "%s", skipping CRUD addons resolver setup.',
-            \is_object($controller) ? $controller::class : 'not_object',
-            AbstractCrudController::class
-        ));
+        // Resolver controller in the closure to avoid unnecessary controller instantiation if no CRUD addons are needed
+        $this->crudAddonsProvider->setResolver(function () use ($controllerFqcn, $actionName, $request): CrudAddons {
+            $controller = $this->controllerFactory->getCrudControllerInstance($controllerFqcn, $actionName, $request);
+
+            return $controller instanceof AbstractCrudController
+                ? $controller->configureCrudAddons()
+                : new CrudAddons();
+        });
     }
 }
